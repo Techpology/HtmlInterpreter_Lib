6a6d058880af090d934d35277c245c118f985e8c
Alfred <54032594+spynetS@users.noreply.github.com>
2021-07-19 19:20:32 +0200

    Function to getAllLines (objF.cs)
    Added function to the getAll lines function on the objF class

--- objF.cs
+++ objF.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace htmlInterpreter.Components
 {
+    
     public class objF : Object
     {
         string Path { get; set; }
@@ -13,10 +15,22 @@ namespace htmlInterpreter.Components
         string Name { get; set; }
         string Extension { get; set; }
 
+        
+        ///<summary>
+        /// Returns all lines in the file as a string
+        ///</summary>
         string getAllLines()
         {
-            //TODO: getAllLines         { Get all lines in current File into a single string }
-            return "";
+            string alllines = "";
+            using(StreamReader sw = new StreamReader(Path+Name+Extension))
+            {
+                string currentline = ""; 
+                while ((currentline = sw.ReadLine())!=null) 
+                {// Loops though all the lines and adds them to a single string
+                    currentline += currentline+"\n";
+                }
+            }
+            return alllines;
         }
 
         string getLineAtIndex(int _index)
