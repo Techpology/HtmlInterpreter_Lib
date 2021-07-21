8adf4f7b45bc1c2e85c645a3a9bdcf2882ad0441
Sharkoo <44705253+SharkooMaster@users.noreply.github.com>
2021-07-21 14:26:14 +0200

    STACS (Standard Caching System) implementation setup. // Preparing plan, diagram done and linked with id 70

---
+++ Caching/STACS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htmlInterpreter.Caching
+{
+    //Id: 70
+    //*2 https://drive.google.com/file/d/1v3lS3j4NBXnTDO1yJsc2uvXlliJgprTQ/view?usp=sharing
+    class STACS
+    {
+
+    }
+}
