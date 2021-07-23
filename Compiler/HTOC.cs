9273fc9f8a0771a28a36fa7834234567ba13f1cc
Sharkoo <44705253+SharkooMaster@users.noreply.github.com>
2021-07-23 22:40:08 +0200

    Finished learning compiler design structure. Now setting up the first class to mark a division section

---
+++ Compiler/HTOC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htmlInterpreter.Compiler
+{
+    public static class HTOC
+    {
+        // Create a dictionary with <string(key), Type(Value[int,string,char,etc...])>
+
+    }
+}
