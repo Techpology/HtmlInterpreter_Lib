71d53f7eb6ff955d997b82ed5c74f77ced137441
Sharkoo <44705253+SharkooMaster@users.noreply.github.com>
2021-07-25 01:38:46 +0200

    Fixed issue (namespace Debug) not included.

---
+++ Debug/Debuger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htmlInterpreter.Debug
+{
+    public static class Debuger
+    {
+        public static void Log(string _toLog)
+        {
+            Logged = _toLog;
+        }
+
+        public static string Logged { get; set; }
+    }
+}
