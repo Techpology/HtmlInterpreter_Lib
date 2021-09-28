c1f86d1ead9c8908485940b310dbeb392bd2a3a1
Sharkoo <44705253+SharkooMaster@users.noreply.github.com>
2021-09-29 00:48:11 +0200

    Vanilla init

---
+++ Compiler/CPFM/Vanilla/VPA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using htmlInterpreter.Compiler.CPFM;
+
+namespace htmlInterpreter.Compiler.CPFM.Vanilla
+{
+    public static class VPA
+    {
+        [DllImport("vanilla.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getTags();
+        static IntPtr cPtr;
+
+        static void _init()
+        {
+            Debug.Debuger.Log("getting C pointer to initiate Vanilla parser");
+            cPtr = getTags();
+
+            Debug.Debuger.Log($"C pointer grabbed succesfully: {(IntPtr)cPtr}");
+        }
+
+        static string[] lines { get; set; }
+        static string filePath { get; set; }
+
+        public static void VanillaParse(string _path)
+        {
+
+        }
+
+        static void fileToLines()
+        {
+            foreach(string line in lines)
+            {
+
+            }
+        }
+
+    }
+}
