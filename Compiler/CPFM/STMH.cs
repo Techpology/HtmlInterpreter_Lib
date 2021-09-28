8d99670a16103b1b4851092826d9b9fd08070995
Sharkoo <44705253+SharkooMaster@users.noreply.github.com>
2021-09-28 23:28:06 +0200

    Initiating transfer to C based parser after lots of research and trial and error.

---
+++ Compiler/CPFM/STMH.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace htmlInterpreter.Compiler.CPFM
+{
+    public static class STMH
+    {
+
+        public static unsafe int strlen(byte* ptrTarget)
+        {
+            int PtrLength = 0;
+            char temp = '\0';
+
+            while(true)
+            {
+                Encoding.UTF8.GetChars(ptrTarget, 1, &temp, 1);
+
+                if (temp == '\0') break;
+
+                PtrLength++;
+                ptrTarget++;
+            }
+
+            return PtrLength;
+        }
+
+    }
+}
